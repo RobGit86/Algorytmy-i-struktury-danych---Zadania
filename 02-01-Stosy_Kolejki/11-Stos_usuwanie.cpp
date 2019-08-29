61cb74997d95b90249f7130b3780d985e72d93a6
RobGit86 <robertblaszak1@gmail.com>
2019-08-29 12:55:21 +0200

    Last update

---
+++ 02-01-Stosy_Kolejki/11-Stos_usuwanie.cpp
@@ -0,0 +1,135 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+struct ele {
+    int dana;
+    ele* nast;
+};
+
+void removeStack(ele* &stos);
+
+void push(ele* &stos, int x);
+int pop(ele* &stos);
+int topEl(ele* stos);
+bool isEmpty(ele* stos);
+
+void initMenu(ele* stos);
+void showStack(ele* stos);
+
+int main() {
+
+    ele* stos = NULL;
+
+    initMenu(stos);
+
+    return 0;
+}
+
+void removeStack(ele* &stos) {
+
+    while(!isEmpty(stos))
+        pop(stos);
+}
+
+
+void push(ele* &stos, int x) {
+
+    ele* nowy = new ele;
+    nowy->dana = x;
+    nowy->nast = stos;
+    stos = nowy;
+}
+
+int pop(ele* &stos) {
+
+    if(stos == NULL) {
+        cout << "STOS JEST PUSTY - OPERACJA NIEDOZWOLONA";
+        return INT_MIN;
+    } else {
+        ele* tmp = stos->nast;
+        int tmp_dana = stos->dana;
+        delete stos;
+        stos = tmp;
+        return tmp_dana;
+    }
+}
+
+int topEl(ele* stos) {
+
+    if(stos == NULL)
+        cout << "STOS JEST PUSTY - OPERACJA NIEDOZWOLONA";
+    else
+        return stos->dana;
+}
+
+bool isEmpty(ele* stos) {
+
+    if(stos == NULL)
+        return true;
+    else
+        return false;
+}
+
+void initMenu(ele* stos) {
+
+    char choice;
+
+    do {
+        cout << "Wybierz operację na stosie\n";
+        cout << "1. Dodaj do stosu\n";
+        cout << "2. Pobierz ze stosu i zwróć jako wartość funkcji\n";
+        cout << "3. Zwróć element bez jego usuwania\n";
+        cout << "4. Sprawdź czy stos jest pusty\n";
+        cout << "5. Wyświetl stos\n";
+        cout << "6. USUŃ STOS Z PAMIĘCI\n";
+        cout << "7. Wyjdź\n\n";
+        cout << "OPCJA: ";
+
+        cin >> choice;
+        cout << endl;
+
+        switch(choice) {
+            case '1': cout << "Podaj daną do wprowadzenia: ";
+                      int tmp;
+                      cin >> tmp;
+                      push(stos, tmp);
+                      cout << "Dodano " << tmp << endl << endl;
+                      break;
+            case '2': cout << "Zdjęto: " << pop(stos) << endl << endl;
+                      break;
+            case '3': cout << "Element na wierzchu: " << topEl(stos) << endl << endl;
+                      break;
+            case '4': if(isEmpty(stos))
+                        cout << "STOS JEST PUSTY" << endl << endl;
+                      else
+                        cout << "STOS NIE JEST PUSTY" << endl << endl;
+                      break;
+            case '5': showStack(stos);
+                      break;
+            case '6': removeStack(stos);
+                      cout << "STOS WYCZYSZCZONO\n\n";
+                      break;
+            case '7': cout << "DO WIDZENIA!";
+                      break;
+
+            default: cout << "ZŁA OPCJA";
+                     break;
+        }
+    } while(choice !='7');
+}
+
+void showStack(ele* stos) {
+
+    if(stos == NULL)
+        cout << "STOS JEST PUSTY!\n\n";
+    else {
+        cout << "STOS: \n";
+        while(stos != NULL) {
+            cout << stos->dana << endl;
+            stos = stos->nast;
+        }
+        cout << endl << endl;
+    }
+}
---
+++ 02-01-Stosy_Kolejki/12-Kolejka_scalanie.cpp
@@ -0,0 +1,134 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+struct ele {
+    int dana;
+    ele* nast;
+};
+
+void mergeQueue()
+
+void add(ele* &poczkol, ele* &konkol, int x);
+int next(ele* &poczkol, ele* &konkol);
+int firstEl(ele* poczkol);
+bool isEmpty(ele* poczkol);
+
+void initMenu(ele* poczkol, ele* konkol);
+void showQueue(ele* poczkol, ele* konkol);
+
+int main() {
+
+    ele* head_first = NULL;
+    ele* tail_second = NULL;
+
+    ele* head_second = NULL;
+    ele* tail = NULL;
+
+    initMenu(head, tail);
+
+    return 0;
+}
+
+void add(ele* &poczkol, ele* &konkol, int x) {
+
+    ele* node = new ele;
+    node->dana = x;
+    node->nast = NULL;
+
+    if(poczkol == NULL && konkol == NULL)
+        poczkol = konkol = node;
+    else {
+        konkol->nast = node;
+        konkol = node;
+    }
+}
+
+int next(ele* &poczkol, ele* &konkol) {
+
+    if(poczkol == NULL && konkol == NULL)
+        return INT_MIN;
+    else {
+        ele* tmp = poczkol->nast;
+        int result = poczkol->dana;
+        delete poczkol;
+        poczkol = tmp;
+        return result;
+    }
+}
+
+int firstEl(ele* poczkol) {
+
+    if(poczkol == NULL) {
+        cout << "KOLEJKA JEST PUSTA - NIEDOZWOLONA OPERACJA ";
+        return INT_MIN;
+    } else
+        return poczkol->dana;
+}
+
+bool isEmpty(ele* poczkol) {
+
+    if(poczkol == NULL)
+        return true;
+    else
+        return false;
+}
+
+void initMenu(ele* poczkol, ele* konkol) {
+
+    char choice;
+
+    do {
+        cout << "Wybierz operację na stosie\n";
+        cout << "1. Dodaj element do kolejki\n";
+        cout << "2. Pobierz pierwszy element i zwróć\n";
+        cout << "3. Zwróć element z początku kolejki bez jego usuwania\n";
+        cout << "4. Sprawdź czy kolejka jest pusta\n";
+        cout << "5. Wyświetl kolejkę\n";
+        cout << "6. Wyjdź\n\n";
+        cout << "OPCJA: ";
+
+        cin >> choice;
+        cout << endl;
+
+        switch(choice) {
+            case '1': cout << "Podaj daną do wprowadzenia: ";
+                      int tmp;
+                      cin >> tmp;
+                      add(poczkol, konkol, tmp);
+                      cout << "Dodano " << tmp << endl << endl;
+                      break;
+            case '2': cout << "Zdjęto: " << next(poczkol, konkol) << endl << endl;
+                      break;
+            case '3': cout << "Element na wierzchu: " << firstEl(poczkol) << endl << endl;
+                      break;
+            case '4': if(isEmpty(poczkol))
+                        cout << "KOLEJKA JEST PUSTA" << endl << endl;
+                      else
+                        cout << "KOLEJKA NIE JEST PUSTA" << endl << endl;
+                      break;
+            case '5': showQueue(poczkol, konkol);
+                      break;
+            case '6': cout << "DO WIDZENIA!";
+                      break;
+
+            default: cout << "ZŁA OPCJA";
+                     break;
+        }
+    } while(choice !='6');
+}
+
+void showQueue(ele* poczkol, ele* konkol) {
+
+    if(poczkol == NULL)
+        cout << "KOLEJKA JEST PUSTA!\n\n";
+    else {
+        cout << "KOLEJKA: \n";
+        while(poczkol != NULL) {
+            cout << poczkol->dana << endl;
+            poczkol = poczkol->nast;
+        }
+        cout << endl << endl;
+    }
+}
